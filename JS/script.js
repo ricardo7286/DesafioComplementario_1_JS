97e781aef0ebdbb2d017a559f775da876018bed2
ricardo7286 <ricardo7286@hotmail.com>
2022-06-01 07:56:25 -0500

    agrego parseint y toLowerCase

--- JS/script.js
+++ JS/script.js
@@ -3,13 +3,13 @@ alert("Hola muy buen dia.\nMi nombre es Ricardo Hernandez Gomez\nBienvenid@ a mi
 //prompt para la variable del nombre del usuario
 let nombre = prompt("Vamos conociendo un poco mas de ti; si estas de acuerdo a continuacion ingresa tu nombre")
 //prompt para la variable de la edad del usuario
-let edad = prompt("Que te parece si tambien ingresas tu edad (años cumplidos)")
+let edad = parseInt(prompt("Que te parece si tambien ingresas tu edad (años cumplidos)"))
 //alert concatenando las variables nombre y edad con un texto amigable
 alert("Wow " + nombre + " tienes " + edad + " años de pura sabiduria.\nEs por ello que te invito a resolver el siguiente enigma de la humanidad")
 //prompt con ciclo para obtener una salida true o false en cada respuesta ingresada
 let pregunta  = prompt("Que fue primero, el huevo o la gallina?");
 //en while la palabra de salida del bucle es huevo
-while (pregunta != "huevo") {
+while (pregunta.toLowerCase() != "huevo") {
     //cualquier otro string introducido alerta al usuario concatenando su nombre y la respuesta a la pregunta
     alert("Upss " + nombre + ", " + pregunta + " No es la respuesta correcta");
     //bucle continuo al no ingresar la respuesta correcta
